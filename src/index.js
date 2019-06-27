1c0c6f7f7b0f81b1994b8be8a9049d5d890cbf29
James Dobbs <jamesdraytondobbs@gmail.com>
2019-06-26 22:30:19 -0500

    using / manipulating state

--- src/index.js
+++ src/index.js
@@ -2,15 +2,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    )
-
-    return <div>Hi there!</div>
-};
 
 
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lat: null, 
+            errMsg: null
+        };
+
+
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({ lat: position.coords.latitude })
+            },
+            err => this.setState({ errMsg: err.message})
+        );
+
+    }
+
+    render() {
+        if (this.state.errMsg && !this.state.lat) {
+            return <div>Error: {this.state.errMsg}</div>
+        } else if (!this.state.errMsg && this.state.lat) {
+            return <div>lat: {this.state.lat}</div>
+        } else {
+            return <div>laoding...</div>
+        }        
+    }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
