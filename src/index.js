f61aee8a9dd0ef4373a7e5936a14333e66eba179
Drayton <Dobbz123@yahoo.com>
2019-06-28 15:32:18 -0500

    completed first react app

--- src/index.js
+++ src/index.js
@@ -1,41 +1,37 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            lat: null, 
-            errMsg: null
-        };
-
-
-
-        window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({ lat: position.coords.latitude })
-            },
-            err => this.setState({ errMsg: err.message})
-        );
-
-    }
+    state = { lat: null, errMsg: '' };
 
     componentDidMount() {
-        console.log('Component was rendered')
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errMsg: err.message })
+        );
     }
 
-    render() {
+    renderContent = () => {
         if (this.state.errMsg && !this.state.lat) {
             return <div>Error: {this.state.errMsg}</div>
         } else if (!this.state.errMsg && this.state.lat) {
-            return <div>lat: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat} />
         } else {
-            return <div>laoding...</div>
-        }        
+            return <Spinner message="Please accept location request" />
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderContent()}
+            </div>
+        )
+
     }
 }
 
